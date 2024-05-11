a2cf57c23045f69dfe03b502d20ad62d5ba62ad4
riku546 <91833521+riku546@users.noreply.github.com>
2024-05-11 13:08:05 +0900

    Update slide_count.py

--- slide_count.py
+++ slide_count.py
@@ -7,13 +7,10 @@ from selenium.webdriver.common.by import By
 
 def slide_count(page_url):
     options = Options()
-# page_url = "https://docs.google.com/presentation/d/e/2PACX-1vRaMwHlOn6-5FvAyqi6puXfEoN8xxzxhfUErJ0WSV85iWoSaJqHqmXlZeYGA0_stg/embed?start=false&loop=false&delayms=3000&slide=id"
-# page_url2 = "https://docs.google.com/presentation/d/e/2PACX-1vQG0Yhr0DnC74095EIDQWIhi8AXSVzlwp4nB-DDn89bD585IcN12-dC5x94k9DDzQ/embed?start=false&loop=false&delayms=3000&slide=id"
-# page_url3 = "https://docs.google.com/presentation/d/e/2PACX-1vQN3CLyNuuYg2B6jT0mZR14sP4zyw3iMkEebNkSB3toTJ1UgT9qAhf6C777hhJ0-A/embed?start=false&loop=false&delayms=3000&slide=id"
-# page_url4 = "https://docs.google.com/presentation/d/e/2PACX-1vSH_eiyPclqpWx-UYKz-kHmOJwHOoggiS5sgGxraVxiXBdfT2u19SWPY3D_4TZSag/embed?start=false&loop=false&delayms=60000&slide=id"
+
     #以下の二つの引数を変更する
-    options.add_argument("--user-data-dir=C:/Users/opa97/AppData/Local/Google/Chrome/User Data/")
-    options.add_argument('--profile-directory=Profile 2')
+    options.add_argument("--user-data-dir=C")
+    options.add_argument('--profile-directory=')
 
     driver = webdriver.Chrome(options=options)
 
